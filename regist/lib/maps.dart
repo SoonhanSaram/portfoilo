81a28212eac02eb6a8a4690023caf66d4c83af1b
SoonhanSaram <kyengmin911@naver.com>
2023-03-21 17:42:18 +0900

    0321

--- regist/lib/maps.dart
+++ regist/lib/maps.dart
@@ -5,7 +5,10 @@ import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:kpostal/kpostal.dart';
 import 'package:location/location.dart';
-import 'package:regist/reselvation.dart';
+import 'package:provider/provider.dart';
+import 'package:regist/result_page.dart';
+import 'package:regist/staticValue/static_value.dart';
+import 'package:regist/viewmodel/booked_view_model.dart';
 
 class Maps extends StatefulWidget {
   const Maps({super.key});
@@ -23,7 +26,7 @@ class MapSampleState extends State<Maps> {
   final CameraPosition _exeception =
       const CameraPosition(target: LatLng(37.3512, 126.5834), zoom: 17.5);
 
-  String postCode = "";
+  String? postCode;
   String address = "";
   dynamic latitude = "";
   dynamic longitude = "";
@@ -80,7 +83,7 @@ class MapSampleState extends State<Maps> {
     // int id = Random().nextInt(100);
     // 한 개의 마커만 사용
     // 마커 2개 사용하기
-    String id = postCode;
+    String id = postCode!;
     setState(() {
       markers
           .add(Marker(position: cordinate, markerId: MarkerId(id.toString())));
@@ -89,6 +92,7 @@ class MapSampleState extends State<Maps> {
 
   @override
   Widget build(BuildContext context) {
+    var bookedViewModel = context.watch<BookedViewModel>();
     return Scaffold(
       body: Stack(
         children: [
@@ -108,36 +112,39 @@ class MapSampleState extends State<Maps> {
               _mapController.animateCamera(CameraUpdate.newLatLng(cordinate));
             },
           ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Center(
-                child: Column(
-                  children: [
-                    searchBox(
-                        context: context,
-                        label: "출발지를 입력해주세요",
-                        controller: _startController),
-                    const SizedBox(height: 10),
-                    searchBox(
-                        context: context,
-                        label: "도착지를 입력해주세요",
-                        controller: _destinationController),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        completeButton(context),
-                      ],
-                    )
-                  ],
+          ConstrainedBox(
+            constraints: BoxConstraints(
+              maxHeight: MediaQuery.of(context).size.height * 0.28,
+              minHeight: MediaQuery.of(context).size.height * 0.28,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Center(
+                  child: Column(
+                    children: [
+                      searchBox(
+                          context: context,
+                          label: StaticValues.mapSearchBox1,
+                          location: bookedViewModel.from,
+                          controller: _startController),
+                      const SizedBox(height: 10),
+                      searchBox(
+                          context: context,
+                          label: StaticValues.mapSearchBox2,
+                          location: bookedViewModel.destination,
+                          controller: _destinationController),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          completeButton(context, bookedViewModel),
+                        ],
+                      )
+                    ],
+                  ),
                 ),
-              ),
-              const Flexible(
-                child: SizedBox(
-                  height: 650,
-                ),
-              ),
-            ],
+              ],
+            ),
           )
         ],
       ),
@@ -146,7 +153,8 @@ class MapSampleState extends State<Maps> {
 
   TextField searchBox({
     required BuildContext context,
-    String label = "출발지를 입력해주세요",
+    required String label,
+    required String location,
     required TextEditingController controller,
   }) {
     return TextField(
@@ -185,16 +193,29 @@ class MapSampleState extends State<Maps> {
     );
   }
 
-  TextButton completeButton(BuildContext context) {
+  TextButton completeButton(
+      BuildContext context, BookedViewModel bookedViewModel) {
     return TextButton(
       onPressed: () {
-        Navigator.push(context, MaterialPageRoute(
-          builder: (context) {
-            // reselInfo.from = _startController.text;
-            // reselInfo.destination = _destinationController.text;
-            return const reselvation();
-          },
-        ));
+        if (_startController.text.isEmpty != true &&
+            _destinationController.text.isEmpty != true) {
+          bookedViewModel.from = _startController.text;
+          bookedViewModel.destination = _destinationController.text;
+          _showBottomSheet(context, bookedViewModel);
+          // print("${bookedViewModel.from}, ${bookedViewModel.destination}");
+        } else if (_startController.text.isEmpty == true) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text(StaticValues.warningMessage1),
+            ),
+          );
+        } else if (_destinationController.text.isEmpty == true) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text(StaticValues.warningMessage2),
+            ),
+          );
+        }
       },
       style: ButtonStyle(
         backgroundColor: MaterialStateProperty.all(Colors.blue),
@@ -205,4 +226,102 @@ class MapSampleState extends State<Maps> {
       ),
     );
   }
+}
+
+void toResult(BuildContext context) {
+  Navigator.push(
+    context,
+    MaterialPageRoute(
+      builder: (context) {
+        return const ResultPage();
+      },
+    ),
+  );
+}
+
+void _showBottomSheet(BuildContext context, BookedViewModel bookedViewModel) {
+  showModalBottomSheet(
+    context: context,
+    builder: (BuildContext context) {
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ListTile(
+            leading: const Icon(Icons.car_rental_outlined),
+            trailing: const Text(
+              "00000원",
+              style: TextStyle(fontSize: 36),
+            ),
+            title: const Text(
+              StaticValues.option1,
+              style: TextStyle(fontSize: 36),
+            ),
+            onTap: () {
+              bookedViewModel.transport = StaticValues.option1;
+              bookedViewModel.fee = "00000";
+              toResult(context);
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.car_rental),
+            trailing: const Text(
+              "00000원",
+              style: TextStyle(fontSize: 36),
+            ),
+            title: const Text(
+              StaticValues.option2,
+              style: TextStyle(fontSize: 36),
+            ),
+            onTap: () {
+              bookedViewModel.transport = StaticValues.option2;
+              bookedViewModel.fee = "00000";
+              toResult(context);
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.car_rental_outlined),
+            trailing: const Text(
+              "00000원",
+              style: TextStyle(fontSize: 36),
+            ),
+            title: const Text(
+              StaticValues.option3,
+              style: TextStyle(fontSize: 36),
+            ),
+            onTap: () {
+              bookedViewModel.transport = StaticValues.option3;
+              bookedViewModel.fee = "00000";
+              toResult(context);
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.car_rental_sharp),
+            trailing: const Text(
+              "00000원",
+              style: TextStyle(fontSize: 36),
+            ),
+            title: const Text(
+              StaticValues.option4,
+              style: TextStyle(fontSize: 36),
+            ),
+            onTap: () {
+              bookedViewModel.transport = StaticValues.option4;
+              bookedViewModel.fee = "00000";
+              toResult(context);
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.cancel),
+            title: const Text(
+              StaticValues.cancel,
+              style: TextStyle(fontSize: 36),
+            ),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      );
+    },
+  );
 }
---
+++ regist/lib/result_page.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:regist/staticValue/static_value.dart';
+import 'package:regist/viewmodel/booked_view_model.dart';
+
+class ResultPage extends StatelessWidget {
+  const ResultPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var bookedViewModel = context.read<BookedViewModel>();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("예약정보 확인"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          infoCard(content: "${bookedViewModel.user}님의 예약정보입니다"),
+          infoCard(
+              content: "${bookedViewModel.resDate} ${bookedViewModel.resTime}"),
+          infoCard(
+              content:
+                  '''출발지 :   ${bookedViewModel.from} \n도착지 :   ${bookedViewModel.destination}'''),
+          infoCard(content: "요금은 ${bookedViewModel.fee}원입니다"),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                  minWidth: MediaQuery.of(context).size.width * 0.8,
+                  maxHeight: MediaQuery.of(context).size.width * 0.8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      StaticValues.cancel,
+                      style: TextStyle(fontSize: 24),
+                    ),
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.5,
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      StaticValues.save,
+                      style: TextStyle(fontSize: 24),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Card infoCard({required String content}) {
+    return Card(
+        elevation: 2,
+        child: Text(
+          content,
+          style: const TextStyle(fontSize: 24),
+        ));
+  }
+}
