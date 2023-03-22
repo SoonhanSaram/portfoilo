62642a702e391fb655cf1710c034cd9f754aed6f
SoonhanSaram <kyengmin911@naver.com>
2023-03-22 17:39:51 +0900

    polyline 수정

--- regist/lib/maps.dart
+++ regist/lib/maps.dart
@@ -1,13 +1,14 @@
 import 'dart:async';
 
 import "package:flutter/material.dart";
-import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:google_directions_api/google_directions_api.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:kpostal/kpostal.dart';
 import 'package:location/location.dart';
 import 'package:provider/provider.dart';
 import 'package:regist/result_page.dart';
 import 'package:regist/staticValue/static_value.dart';
+import 'package:regist/ui_modules/ui_modules.dart';
 import 'package:regist/viewmodel/booked_view_model.dart';
 
 class Maps extends StatefulWidget {
@@ -18,11 +19,16 @@ class Maps extends StatefulWidget {
 }
 
 class MapSampleState extends State<Maps> {
+  late double lat1;
+  late double lng1;
+  late double lat2;
+  late double lng2;
+
   Location location = Location();
   LatLng? _latLng;
   CameraPosition? _kGooglePlex;
   final List<Marker> _marker = [];
-
+  double distance = 0;
   final CameraPosition _exeception =
       const CameraPosition(target: LatLng(37.3512, 126.5834), zoom: 17.5);
 
@@ -34,37 +40,107 @@ class MapSampleState extends State<Maps> {
   late GoogleMapController _mapController;
   final _startController = TextEditingController();
   final _destinationController = TextEditingController();
-  final Set<Polyline> _polylines = <Polyline>{};
-  PolylinePoints polylinePoints = PolylinePoints();
+  late Set<Polyline> polylines = {};
+  late List<LatLng> latLng = [];
+  // PolylinePoints polylinePoints = PolylinePoints();
+  // final PolylineResult _polyResult = PolylineResult(points: []);
+
+  Future<void> getPolylines(String location1, String location2) async {
+    DirectionsService.init("AIzaSyBlvArPjmzxG13H7YJTzglJK3KH4pU3ByQ");
+
+    final directionsService = DirectionsService();
+
+    final request = DirectionsRequest(
+      origin: location1,
+      destination: location2,
+      travelMode: TravelMode.driving,
+    );
+    print(request);
+
+    try {
+      final response = await directionsService.route(request,
+          (DirectionsResult response, DirectionsStatus? status) {
+        if (status == DirectionsStatus.ok) {
+          print(response);
+        } else {
+          print("object");
+        }
+      });
+    } catch (e) {
+      print("error $e");
+    }
+    print("최종");
+  }
+
+  Future<void> _fitBounds() async {
+    final LatLngBounds bounds = LatLngBounds(
+      southwest: latLng[0],
+      northeast: latLng[1],
+    );
+
+    final GoogleMapController mapController = await _controller.future;
+    mapController.animateCamera(
+      CameraUpdate.newLatLngBounds(
+        bounds,
+        50,
+      ),
+    );
+  }
+
+  Future<void> getperMission() async {
+    bool serviceEnabled;
+    PermissionStatus permissionGrandted;
+
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+    }
+    if (!serviceEnabled) {
+      return;
+    }
+
+    permissionGrandted = await location.hasPermission();
+    if (permissionGrandted == PermissionStatus.denied) {
+      permissionGrandted = await location.requestPermission();
+    }
+    if (permissionGrandted != PermissionStatus.granted) {
+      return;
+    }
+    Timer(const Duration(seconds: 1), () {
+      Location.instance.onLocationChanged.listen(
+        (LocationData currentLocation) {
+          if (currentLocation.latitude != null &&
+              currentLocation.longitude != null) {
+            setState(
+              () {
+                _latLng = LatLng(
+                    currentLocation.latitude!, currentLocation.longitude!);
+
+                _kGooglePlex = CameraPosition(
+                  target: _latLng!,
+                  zoom: 17.5,
+                );
+                _marker.add(
+                  Marker(
+                    markerId: const MarkerId("내 위치"),
+                    draggable: true,
+                    position: _latLng!,
+                  ),
+                );
+              },
+            );
+          } else {
+            _latLng = const LatLng(37.3512, 126.5834);
+          }
+        },
+      );
+    });
+  }
 
   @override
   void initState() {
     super.initState();
-    Location.instance.onLocationChanged.listen(
-      (LocationData currentLocation) async {
-        if (currentLocation.latitude != null &&
-            currentLocation.longitude != null) {
-          setState(
-            () {
-              _latLng =
-                  LatLng(currentLocation.latitude!, currentLocation.longitude!);
-
-              _kGooglePlex = CameraPosition(
-                target: _latLng!,
-                zoom: 17.5,
-              );
-              _marker.add(
-                Marker(
-                  markerId: const MarkerId("내 위치"),
-                  draggable: true,
-                  position: _latLng!,
-                ),
-              );
-            },
-          );
-        }
-      },
-    );
+    getperMission();
   }
 
   @override
@@ -78,12 +154,12 @@ class MapSampleState extends State<Maps> {
       Completer<GoogleMapController>();
 
   final List<Marker> markers = [];
-  addMarker(cordinate) {
+  addMarker(cordinate, address) {
     // marker 여러개 사용시
     // int id = Random().nextInt(100);
     // 한 개의 마커만 사용
     // 마커 2개 사용하기
-    String id = postCode!;
+    String id = address!;
     setState(() {
       markers
           .add(Marker(position: cordinate, markerId: MarkerId(id.toString())));
@@ -92,11 +168,14 @@ class MapSampleState extends State<Maps> {
 
   @override
   Widget build(BuildContext context) {
+    var uiModules = UiModules();
     var bookedViewModel = context.watch<BookedViewModel>();
     return Scaffold(
       body: Stack(
         children: [
           GoogleMap(
+            polylines: polylines,
+            markers: Set.from(markers),
             zoomControlsEnabled: false,
             zoomGesturesEnabled: true,
             myLocationEnabled: true,
@@ -106,8 +185,11 @@ class MapSampleState extends State<Maps> {
               setState(() {
                 _mapController = controller;
               });
+              if (latLng.length == 2) {
+                _controller.complete(controller);
+                _fitBounds();
+              }
             },
-            // markers: Set.from(_marker),
             onTap: (cordinate) {
               _mapController.animateCamera(CameraUpdate.newLatLng(cordinate));
             },
@@ -127,13 +209,25 @@ class MapSampleState extends State<Maps> {
                           context: context,
                           label: StaticValues.mapSearchBox1,
                           location: bookedViewModel.from,
-                          controller: _startController),
+                          controller: _startController,
+                          uiModules: uiModules,
+                          polyResult: () => getPolylines(bookedViewModel.from,
+                              bookedViewModel.destination),
+                          bookedViewModel: bookedViewModel,
+                          place: bookedViewModel.from),
                       const SizedBox(height: 10),
                       searchBox(
                           context: context,
                           label: StaticValues.mapSearchBox2,
                           location: bookedViewModel.destination,
-                          controller: _destinationController),
+                          controller: _destinationController,
+                          uiModules: uiModules,
+                          polyResult: () => getPolylines(
+                                bookedViewModel.from,
+                                bookedViewModel.destination,
+                              ),
+                          bookedViewModel: bookedViewModel,
+                          place: bookedViewModel.destination),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.end,
                         children: [
@@ -145,7 +239,26 @@ class MapSampleState extends State<Maps> {
                 ),
               ],
             ),
-          )
+          ),
+          distance.isFinite == true && distance > 0
+              ? Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(width: 1),
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(8)),
+                        ),
+                        child: Text("$distance KM"),
+                      ),
+                    ],
+                  ),
+                )
+              : Container(),
         ],
       ),
     );
@@ -156,6 +269,10 @@ class MapSampleState extends State<Maps> {
     required String label,
     required String location,
     required TextEditingController controller,
+    required UiModules uiModules,
+    required Function polyResult,
+    required BookedViewModel bookedViewModel,
+    String? place,
   }) {
     return TextField(
       controller: controller,
@@ -172,10 +289,62 @@ class MapSampleState extends State<Maps> {
                 latitude = result.latitude!.toString();
                 longitude = result.longitude!.toString();
                 searchedPosition = LatLng(result.latitude!, result.longitude!);
+                place = address;
                 _mapController
                     .animateCamera(CameraUpdate.newLatLng(searchedPosition));
-                addMarker(searchedPosition);
+                addMarker(searchedPosition, address);
                 controller.text = address;
+                latLng.add(searchedPosition);
+
+                setState(() async {
+                  if (latLng.length == 2) {
+                    distance = 0;
+
+                    late List latingArr;
+                    late String latlngStr;
+                    late List locationArr = [];
+                    try {
+                      for (var i = 0; i < latLng.length; i++) {
+                        latlngStr = latLng[i].toString();
+                        latingArr = latlngStr.split(
+                          RegExp(r'[,\s()]'),
+                        );
+                        locationArr.add([...latingArr]);
+                      }
+                    } catch (e) {
+                      print(e);
+                    }
+
+                    lat1 = double.parse(locationArr[0][1]);
+                    lng1 = double.parse(locationArr[0][3]);
+                    lat2 = double.parse(locationArr[1][1]);
+                    lng2 = double.parse(locationArr[1][3]);
+                    distance += uiModules.distance(lat1, lng1, lat2, lng2);
+                    if (bookedViewModel.from.isNotEmpty &&
+                        bookedViewModel.destination.isNotEmpty) {
+                      polyResult(
+                          bookedViewModel.from, bookedViewModel.destination);
+                      print('경로 $polyResult');
+                    }
+
+                    // Polyline pathPolyline = Polyline(
+                    // polylineId: const PolylineId("path"),
+                    // points: polyResult.
+                    // .map(
+                    // (e) => LatLng(e.latitude, e.longitude),
+                    // )
+                    // .toList(),
+                    // width: 2,
+                    // color: Colors.orangeAccent);
+                    // print("path : $pathPolyline");
+                    // polylines.add(pathPolyline);
+                    // print("여기");
+                    // debugPrint(polylines.toString());
+                  } else if (latLng.length > 2) {
+                    distance = 0;
+                    latLng = [];
+                  }
+                });
               },
             ),
           ),
@@ -199,8 +368,6 @@ class MapSampleState extends State<Maps> {
       onPressed: () {
         if (_startController.text.isEmpty != true &&
             _destinationController.text.isEmpty != true) {
-          bookedViewModel.from = _startController.text;
-          bookedViewModel.destination = _destinationController.text;
           _showBottomSheet(context, bookedViewModel);
           // print("${bookedViewModel.from}, ${bookedViewModel.destination}");
         } else if (_startController.text.isEmpty == true) {
--- regist/lib/ui_modules/ui_modules.dart
+++ regist/lib/ui_modules/ui_modules.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:regist/calendar.dart';
 import 'package:regist/maps.dart';
@@ -25,5 +28,13 @@ class UiModules {
         },
       ),
     );
+  }
+
+  double distance(lat1, lon1, lat2, lon2) {
+    var p = 0.017453292519943295;
+    var a = 0.5 -
+        cos((lat2 - lat1) * p) / 2 +
+        cos(lat1 * p) * cos(lat2 * p) * (1 - cos((lon2 - lon1) * p)) / 2;
+    return 12742 * asin(sqrt(a));
   }
 }
